26b287c10e46265ce37e8844e0afdbc49b660bb9
Trip Daria <daria.trip001@gmail.com>
2020-10-05 16:22:29 +0300

    adaugat cod pentru citire date de la consola

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,26 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World of C#!");
+            Console.WriteLine("Introduceti numarul de la pantof: ");
+            string line;
+            line = Console.ReadLine();
+            int numarPantof;
+            try
+            {
+                numarPantof = int.Parse(line);
+                //int.TryParse(line, out numarPantof);
+                Console.WriteLine($"Aveti{numarPantof} numarul la pantof");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            } 
+
+            
+           
+
+            
+
         }
     }
 }
